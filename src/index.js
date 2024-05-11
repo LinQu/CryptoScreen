46cc75e8b7a2942329adaaca290263009847ce1d
LinQu <asirayudha11@gmail.com>
2024-05-11 15:33:44 +0700

    commit index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  HashRouter,
+  Route,
+  Switch,
 } from "react-router-dom";
 import Home from './pages/Home';
 import Crypto from './pages/Crypto';
@@ -11,56 +12,17 @@ import Trending from './pages/Trending';
 import Saved from './pages/Saved';
 import CryptoDetails from './components/CryptoDetails';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    children:[
-      {
-        path:"/",
-        element: <Crypto />,
-        children: [
-          {
-            path:":coinId",
-            element: <CryptoDetails />
-          }
-        ]
-      },
-      {
-        path:"/trending",
-        element: <Trending />,
-        children: [
-          {
-            path:":coinId",
-            element: <CryptoDetails />
-          }
-        ]
-      },
-      {
-        path:"/saved",
-        element: <Saved />,
-        children: [
-          {
-            path:":coinId",
-            element: <CryptoDetails />
-          }
-        ]
-      }
-    ]
-
-
-  },
-]);
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/crypto" component={Crypto} />
+        <Route exact path="/trending" component={Trending} />
+        <Route exact path="/saved" component={Saved} />
+        <Route exact path="/crypto/:coinId" component={CryptoDetails} />
+      </Switch>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
